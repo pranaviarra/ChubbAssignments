225d1efda8897132b540261a24ecdf2f399be652
saipranavi <aspranavi1110@gmail.com>
2026-01-03 22:05:08 +0530

    modified DAG file

--- Capstone/Airflow DAG files/global_trade_pipeline.py
+++ Capstone/Airflow DAG files/global_trade_pipeline.py
@@ -1,12 +1,18 @@
 from airflow import DAG
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator
+
+default_args = {
+    "retries": 3,                         # Retry 3 times
+    "retry_delay": timedelta(minutes=5),  # Wait 5 mins between retries
+}
 
 with DAG(
     dag_id="global_trade_pipeline",
     start_date=datetime(2023, 1, 1),
     schedule_interval=None,
     catchup=False,
+    default_args=default_args,
     description="Bronze -> Silver -> Gold Databricks Pipeline",
     tags=["capstone", "databricks"]
 ) as dag:
@@ -30,4 +36,3 @@ with DAG(
     )
 
     bronze_task >> silver_task >> gold_task
-
